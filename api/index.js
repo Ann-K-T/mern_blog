3653cc8ca57db1479d3c8d729f0ab917dc031705
Ann Thomas <ann-thomas2010@hotmail.com>
2025-02-16 18:18:21 -0500

    add middleware & function to handle errors

--- api/index.js
+++ api/index.js
@@ -27,3 +27,13 @@ app.use('/api/user', userRoutes);
 // .use is a method that allows you to run a function on every request that matches the path
 // This is a middleware function that will run for every request that matches /api/user
 app.use('/api/auth', authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
