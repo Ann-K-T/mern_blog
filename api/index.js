371bc52a11a55404777517719880c8de5a2602cf
Ann Thomas <ann-thomas2010@hotmail.com>
2025-02-16 17:55:45 -0500

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -15,6 +16,8 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
+//.json() is a method inbuilt in express to recognize the incoming Request Object as a JSON Object.
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!!');
@@ -23,3 +26,4 @@ app.listen(3000, () => {
 app.use('/api/user', userRoutes);
 // .use is a method that allows you to run a function on every request that matches the path
 // This is a middleware function that will run for every request that matches /api/user
+app.use('/api/auth', authRoutes);
